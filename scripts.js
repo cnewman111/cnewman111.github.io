199689f34a9945b94d0323b2a5846a6c880800a9
Colin Newman <colinnewman100@gmail.com>
2023-08-13 14:19:23 -0700

    Dynamic navbar

--- scripts.js
+++ scripts.js
@@ -20,3 +20,18 @@ window.addEventListener('scroll', () => {
         } 
     });
 });
+
+const navbar = document.querySelector('.navbar');
+let clear = true
+
+window.addEventListener('scroll', () => {
+    if (clear && window.scrollY > navbar.offsetHeight) {
+        navbar.classList.add('bg-dark')
+        clear = false
+    }
+    else if (!clear && window.scrollY <= navbar.offsetHeight) {
+        navbar.classList.remove('bg-dark')
+        clear = true
+    }
+
+});
