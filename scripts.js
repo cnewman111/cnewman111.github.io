a1e19c4fccba7a42a811bd7ac83329a7ab7e3abd
Colin Newman <colinnewman100@gmail.com>
2023-08-14 00:13:26 -0700

    Added all sections, still working on polish

--- scripts.js
+++ scripts.js
@@ -1,29 +1,42 @@
+//"Active" appearence corresponding to the section user scrolls to
 const navLinkEls = document.querySelectorAll('.nav-link');
 const sectionEls = document.querySelectorAll('section');
+const landing = document.querySelector('#landing')
 
 let currentSection = 'about';
-
 window.addEventListener('scroll', () => {
+    let assigned = false
     sectionEls.forEach(sectionEl => {
         if (window.scrollY >= sectionEl.offsetTop - sectionEl.offsetHeight / 2) {
             currentSection = sectionEl.id;
+            assigned = true
         }
     });
 
-    navLinkEls.forEach(navLinkEl => {
-        if (navLinkEl.href.includes(currentSection)) {
-            const activeElement = document.querySelector('.active');
-            if (activeElement) {
-                activeElement.classList.remove('active');
-            }
-            navLinkEl.classList.add('active');
-        } 
-    });
+    if (window.scrollY <= landing.offsetHeight / 2) {
+        currentSection = null
+        const activeElement = document.querySelector('.active');
+        if (activeElement) {
+            activeElement.classList.remove('active');
+        }
+    }
+    else {
+        navLinkEls.forEach(navLinkEl => {
+            if (navLinkEl.href.includes(currentSection)) {
+                const activeElement = document.querySelector('.active');
+                if (activeElement) {
+                    activeElement.classList.remove('active');
+                }
+                navLinkEl.classList.add('active');
+            } 
+        });
+    }
 });
 
+
+//Clear to opaque navbar on scroll
 const navbar = document.querySelector('.navbar');
 let clear = true
-
 window.addEventListener('scroll', () => {
     if (clear && window.scrollY > navbar.offsetHeight) {
         navbar.classList.add('bg-dark')
